112d8e156f1adb84fd0dfed5e2e3253e798fd056
Eve Blakcori <eve.blakcori@googlemail.com>
2022-06-13 16:23:34 +0100

    Renamed the utils folder

--- __scaffolding-tool/make-component.js
+++ __scaffolding-tool/make-component.js
@@ -26,7 +26,12 @@ function createStylesFile(path, componentName) {
     // Styles file
     fs.writeFileSync(
         `${path}/${componentName}.scss`,
-        `// This is a comment to start ${componentName} styles`
+        `// This is a comment to start ${componentName} styles
+.ui-${componentName} {
+    // Your styles here
+    // Use BEM
+}
+        `
     );
 }
 
